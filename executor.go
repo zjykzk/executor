a8f9eba4d7861e6efc173c3941edf2efb3bc0730
zjykzk <zjykzk@gmail.com>
2018-01-07 22:13:52 +0800

    X

--- executor.go
+++ executor.go
@@ -1,6 +1,28 @@
-package gexe
+package executor
+
+import (
+	"sync"
+	"time"
+)
 
 // Executor executes the submitted tasks
 type Executor interface {
 	Execute(r Runnable)
+}
+
+type workerChan struct {
+	lastUseTime time.Time
+	ch          chan Runnable
+}
+
+// GoroutinePoolExecutor executor with pooled goroutie
+type GoroutinePoolExecutor struct {
+	corePoolSize, maximumPoolSize int32
+	keeyLiveTime                  time.Duration
+
+	ready []*workerChan
+
+	stopCh chan struct{}
+
+	workerChanPool sync.Pool
 }
--- runnable.go
+++ runnable.go
@@ -1,4 +1,4 @@
-package gexe
+package executor
 
 // Runnable common protocol for objects to be executed
 type Runnable interface {
